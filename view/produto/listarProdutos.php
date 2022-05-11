df4d681dd68eb232448514846cadd177f40a3d44
Lessa Apolinario <lessaapolinario@gmail.com>
2022-05-10 22:00:21 -0300

    Add list method logic for listarProdutos view

--- view/produto/listarProdutos.php
+++ view/produto/listarProdutos.php
@@ -1,9 +1,30 @@
+<?php
+if ($_REQUEST) {
+    $produtos = $_REQUEST["produtos"];
+
+    foreach ($produtos as $produto) {
+        $html = "
+            <div class='produto'>
+                <p>" . $produto['nome'] . "</p>
+                <div>" . $produto['descricao'] . "</div>
+                <p>" . $produto['preco'] . "</p>
+                <p>" . $produto['caminho_imagem'] . "</p>
+                <p>" . $produto['categoria'] . "</p>
+                <p>" . $produto['quantidade'] . "</p>
+                <p>" . $produto['ncm'] . "</p>
+            </div>
+        ";
+
+        echo $html;
+    }
+}
+?>
 <html>
 
 <body>
     <form action="../../index.php?classe=Produto&acao=list" method="POST">
-        <label for="deletar">Deletar produto</label>
-        <input type="submit" id="deletar" />
+        <label for="listar">Produtos</label>
+        <input type="submit" id="listar" />
     </form>
 </body>
 
